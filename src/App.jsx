46136f8625c628a76b993ec12f686491fc9c338e
Lnvy65 <bike6tonggi@gmail.com>
2026-03-18 12:46:47 +0900

    이게되네
    {Profile()}
    <Profile />

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 //import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 
+function Profile() {
+  return(
+    <>
+      <img src={reactLogo} alt="리액트 로고" style={{ height : 60}} />
+    </>
+  )
+}
+
 export default function App() {
   
   return (
     <>
-      <img src={reactLogo} alt="리액트 로고" style={{ height : 60}} />
+      {Profile()}
+      <Profile />
     </>
   )
 }
