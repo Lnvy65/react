b03517a1ee27645220b18666adb833e16209a681
Lnvy65 <bike6tonggi@gmail.com>
2026-03-18 12:48:58 +0900

    Profile 분리

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,5 @@
 //import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-
-function Profile() {
-  return(
-    <>
-      <img src={reactLogo} alt="리액트 로고" style={{ height : 60}} />
-    </>
-  )
-}
+import Profile from "./Profile"
 
 export default function App() {
   
