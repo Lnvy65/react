d1efd86c4ad4b0856b6f21c00c58c478225ced3c
Lnvy65 <bike6tonggi@gmail.com>
2026-04-01 12:47:58 +0900

    App에 적용

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@
 import Profile from "./Profile"
 import UseJsx from "./components/UseJsx"
 import ParentComp from "./components/ParentComp"
+import SpreadComp from "./components/SpreadComp"
 
 export default function App() {
   
   return (
     <>
+      <Profile />
+      <UseJsx />
       <ParentComp />
+      <SpreadComp />
     </>
   )
 }
