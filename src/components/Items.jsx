c3400ee58399e92ad0fae95dab76eee2a51e68ee
Lnvy65 <bike6tonggi@gmail.com>
2026-04-08 12:43:20 +0900

    items수정 heroes 추가

--- src/components/Items.jsx
+++ src/components/Items.jsx
@@ -1,7 +1,23 @@
 export default function Items({name, isPacked}) {
+    let itemContent = name;
+    if(isPacked) {
+        itemContent = <del>{itemContent + ' ✔'}</del>;
+    }
+
     return(
         <>
-            <li>{name} {isPacked?  ' ✔' : ""}</li>
+            {/* <li>{name} {isPacked?  ' ✔' : ""}</li> */}
+            {/* <li>
+                {
+                    isPacked ?
+                    <del>
+                        {name + ' ✔'}
+                    </del>
+                    :
+                    name
+                }
+            </li> */}
+            <li>{itemContent}</li>
         </>
     );
 }
