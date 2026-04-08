f193a0f696fcf2aa722ba0c3f941a77630fbd7a4
Lnvy65 <bike6tonggi@gmail.com>
2026-04-08 10:22:24 +0900

    App 수정 및 컴포넌트 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import Profile from './components/Profile'
 import Gallery from './components/Gallery'
+import { NamedComponent1, NamedComponent2, NamedComponent3 } from './components/NamedComponents'
+import { NamedExportTest } from './components/NamedComponentTest'
 
 export default function App() {
 
@@ -7,6 +9,7 @@ export default function App() {
     <>
       <Gallery />
       <Profile />
+      <NamedExportTest />
     </>
   )
 }
