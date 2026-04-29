615ed3a3f5da8477fe93a7c077dd2fbac1236b6c
Lnvy65 <bike6tonggi@gmail.com>
2026-04-29 12:46:59 +0900

    04_29수업내용 저장 기존 내용 삭제

--- src/components/ChildComp.jsx
+++
@@ -1,7 +0,0 @@
-export default function ChildComp({imageInfo, width, height}) {
-    return(
-        <>
-            <img className="button-icon" src={imageInfo.src} alt={imageInfo.alt} width={width} height={height} />
-        </>
-    )
-}
