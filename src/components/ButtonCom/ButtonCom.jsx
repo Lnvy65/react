e684b208d77eb20a68355868686093bc634b8ddc
Lnvy65 <bike6tonggi@gmail.com>
2026-05-06 12:49:54 +0900

    기존 수업내용 복원

--- src/components/ButtonCom/ButtonCom.jsx
+++ src/components/ButtonCom/ButtonCom.jsx
@@ -1,4 +1,25 @@
 {/*
+import style from "./ButtonCom.module.css"
+
+export default function ButtonCom() {
+
+    const handleClick = () => {
+        alert("버튼 클릭")
+    }
+
+  return (
+    <>
+        <h1 className={style.title}>My App</h1>
+        <nav className={style.navBar}>
+            <button className={style.myButton} onClick={handleClick}>Click Me</button>
+            <button className={style.myButton} onClick={handleClick}>Click Me</button>
+        </nav>
+    </>
+  )
+}
+*/}
+
+{/*
 export default function ButtonCom({ message, children }) {
 
     const handleClick = () => {
