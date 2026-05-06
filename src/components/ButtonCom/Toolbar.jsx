e6c5859da82de5234796c7967f4b7132310f7583
Lnvy65 <bike6tonggi@gmail.com>
2026-05-06 12:30:43 +0900

    mp4 기능 추가

--- src/components/ButtonCom/Toolbar.jsx
+++ src/components/ButtonCom/Toolbar.jsx
@@ -1,6 +1,7 @@
-import {handleClick} from "./handle"
+import {handleClick, handlePlay, handleStop} from "./handle"
 import ButtonCom from "./ButtonCom"
 import style from "../../css/ButtonCom.module.css"
+import sampleVideo from "../../assets/348057_medium.mp4"
 
 {/*
 export default function Toolbar() {
@@ -17,15 +18,36 @@ export default function Toolbar() {
 }
 */}
 
+{/*
 export default function Toolbar() {
     return(
         <>
-            <ButtonCom message="버튼1 클릭" handle={handleClick} style={style}>
+            <ButtonCom message="버튼1 클릭" handle={handleClick} style={style.myButton}>
                 버튼1
             </ButtonCom>
-            <ButtonCom message="버튼2 클릭" handle={handleClick} style={style}>
+            <ButtonCom message="버튼2 클릭" handle={handleClick} style={style.myButton}>
                 버튼2
             </ButtonCom>
+        </>
+    )
+}
+*/}
+
+export default function Toolbar() {
+    return(
+        <>
+            <nav>
+                <ButtonCom message="videoPlayer" handle={handlePlay} style={style.myButton} >
+                    Play
+                </ButtonCom>
+                <ButtonCom message="videoPlayer" handle={handleStop} style={style.myButton} >
+                    Stop
+                </ButtonCom>
+            </nav>
+            <br />
+            <section>
+                <video id="videoPlayer" src={sampleVideo} controls width="350" />
+            </section>
         </>
     )
 }
