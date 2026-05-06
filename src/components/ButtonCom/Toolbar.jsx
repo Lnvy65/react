cdedccdba072f78abc04afa67758589d0e13caab
Lnvy65 <bike6tonggi@gmail.com>
2026-05-06 12:02:26 +0900

    handle추가

--- src/components/ButtonCom/Toolbar.jsx
+++ src/components/ButtonCom/Toolbar.jsx
@@ -1,5 +1,7 @@
+import {handleClick} from "./handle"
 import ButtonCom from "./ButtonCom"
 
+{/*
 export default function Toolbar() {
     return(
         <>
@@ -7,6 +9,20 @@ export default function Toolbar() {
                 버튼1
             </ButtonCom>
             <ButtonCom message="버튼2 클릭">
+                버튼2
+            </ButtonCom>
+        </>
+    )
+}
+*/}
+
+export default function Toolbar() {
+    return(
+        <>
+            <ButtonCom message="버튼1 클릭" handle={handleClick}>
+                버튼1
+            </ButtonCom>
+            <ButtonCom message="버튼2 클릭" handle={handleClick}>
                 버튼2
             </ButtonCom>
         </>
