784d9620546125a44aec43547e680fff7e073e77
Lnvy65 <bike6tonggi@gmail.com>
2026-05-13 11:45:27 +0900

    delete unuse components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,4 @@
 //import { useState } from 'react'
-import Profile from "./Profile"
-import UseJsx from "./components/UseJsx"
-import ParentComp from "./components/ParentComp"
-import SpreadComp from "./components/SpreadComp"
-import ButtonCom from "./components/ButtonCom/ButtonCom"
-import Toolbar from "./components/ButtonCom/Toolbar"
 import Bubble from "./components/Bubble"
 
 export default function App() {
