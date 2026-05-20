47dc208b6e35b428c1af781e34457c00c8f3a0af
Lnvy65 <bike6tonggi@gmail.com>
2026-05-20 12:30:20 +0900

    05월 20일 이외 수업내용 삭제

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,9 @@
-//import { useState } from 'react'
-import Profile from "./Profile"
-import UseJsx from "./components/UseJsx"
-import ParentComp from "./components/ParentComp"
-import SpreadComp from "./components/SpreadComp"
-import ButtonCom from "./components/ButtonCom/ButtonCom"
-import Toolbar from "./components/ButtonCom/Toolbar"
-import Bubble from "./components/Bubble"
-import Signup1 from "./components/Signup1"
-import Signup2 from "./components/Signup2"
 import Carousel from "./components/Carousel/Carousel"
 
 export default function App() {
   
   return (
     <>
-      {/* <Profile />
-      <UseJsx />
-      <ParentComp />
-      <SpreadComp /> */}
-      {/* <ButtonCom /> */}
-      {/* <Toolbar /> */}
-      {/* <Bubble /> */}
-      {/* <Signup1 />
-      <Signup2 /> */}
       <Carousel />
     </>
   )
