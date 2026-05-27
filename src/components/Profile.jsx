917d215aa51e465a8af26b3c6a8ee2923ba7c0c6
Lnvy65 <bike6tonggi@gmail.com>
2026-05-27 12:28:15 +0900

    필요없는 파일 삭제

--- src/components/Profile.jsx
+++
@@ -1,9 +0,0 @@
-import reactLogo from '../assets/react.svg'
-
-export default function Profile(){
-    return(
-        <>
-            <img className="logo" src={reactLogo} alt="" style={{ height : 60}} />
-        </>
-    )
-}
